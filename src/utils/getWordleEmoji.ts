aabf3558eb224bee7619888a4e924cee8ffe2885
Miguel Sablan <miguelsablan1@outlook.com>
2024-05-03 01:10:34 -0400

    shareable guess journey emoji function

---
+++ src/utils/getWordleEmoji.ts
@@ -0,0 +1,32 @@
+import { compareGuessAndCorrect } from "./compareGuessAndCorrect";
+
+export const getWordleEmoji = (word: string, guessList: string[]): string => {
+  const MAX_GUESSES = 6;
+  let output = `Wordle ${guessList.length}/${MAX_GUESSES}\n\n`;
+
+  guessList.forEach((guess) => {
+    let line = "";
+    const styles = compareGuessAndCorrect(guess, word);
+
+    for (let i = 0; i < guess.length; i++) {
+      switch (styles[i]) {
+        case "guessCorrect":
+          line += "🟩"; // Green for correct letters in the correct position
+          break;
+        case "guessInWord":
+          line += "🟨"; // Yellow for correct letters in the wrong position
+          break;
+        case "guessNotInWord":
+          line += "⬜"; // Gray for incorrect letters
+          break;
+        default:
+          line += "⬜"; // Gray for any other case
+          break;
+      }
+    }
+
+    output += line + "\n";
+  });
+
+  return output;
+};
